f08c968b03a339caf1214a484c443a4487f54fcf
Aleyna Barut <aleyna._.barut@hotmail.com>
2023-04-26 20:19:17 +0300

    Update game.js

--- game.js
+++ game.js
@@ -1,51 +1,123 @@
-const gameBoard = document.querySelector("#game-board");
-const scoreDisplay = document.querySelector("#score");
-const fruits = ["image/apple.png","image/muz.png","image/orange.png","image/karpuz.png"];
+
+const canvasWidth = 1920;
+const canvasHeight = 1080;
+const canvas = document.getElementById("gameCanvas");
+const ctx = canvas.getContext("2d");
+
+const fruitImages = [
+	{ src: "../images/fruits/apple.png", width: 90, height: 90 },
+	{ src: "../images/fruits/muz.png", width: 80, height: 80 },
+	{ src: "../images/fruits/orange.png", width: 100, height: 100 },
+];
 
 let score = 0;
 let fruitsCollected = 0;
+let gameRunning = true;
+let fruits = [];
+canvas.width = canvasWidth;
+canvas.height = canvasHeight;
+document.addEventListener("keydown", handleKeyDown);
+canvas.addEventListener("mousedown", handleMouseDown);
+setInterval(gameLoop, 1000/60);
 
-function createFruit() {
-  const randomFruit = fruits[Math.floor(Math.random() * fruits.length)];
-  const fruit = document.createElement("img");
-  fruit.setAttribute("src", `${randomFruit}`);
-  fruit.setAttribute("alt", randomFruit);
-  fruit.setAttribute("class", "fruit");
-  fruit.style.left = `${Math.floor(Math.random() * 500) + 50}px`;
-  fruit.style.top = `${Math.floor(Math.random() * 500) + 50}px`;
-  fruit.addEventListener("click", collectFruit);
-  return fruit;
+function gameLoop() {
+	ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+	for (let i = 0; i < fruits.length; i++) {
+		const fruit = fruits[i];
+		ctx.drawImage(fruit.image, fruit.x, fruit.y, fruit.width, fruit.height);
+	}
+
+	document.getElementById("scoreDiv").innerText = "Score: " + score + " / " + fruitsCollected;
+
+	// Yeni meyve oluşturma
+	if (fruits.length < 10 && Math.random() < 0.05) {
+		const fruitImage = fruitImages[Math.floor(Math.random() * fruitImages.length)];
+		fruits.push({
+			image: new Image(),
+			x: Math.random() * (canvasWidth - fruitImage.width),
+			y: Math.random() * (canvasHeight - fruitImage.height),
+			width: fruitImage.width,
+			height: fruitImage.height
+		});
+		fruits[fruits.length - 1].image.src = fruitImage.src;
+	}
+
+	// Oyun bitiş kontrolü
+	if (score >= 20) {
+		gameRunning = false;
+        gameOver();
+          
+	}
 }
 
+function handleKeyDown(e) {
+	// Yön tuşları ile hareket
+	if (e.keyCode === 37) { // Sol tuş
+		moveFruits(-10, 0);
+	}
+	if (e.keyCode === 38) { // Yukarı tuş
+		moveFruits(0, -10);
+	}
+	if (e.keyCode === 39) { // Sağ tuş
+		moveFruits(10, 0);
+	}
+	if (e.keyCode === 40) { // Aşağı tuş
+		moveFruits(0, 10);
+    }
+    // Space tuşu ile meyve toplama
+if (e.keyCode === 32) { // Space tuşu
+	for (let i = 0; i < fruits.length; i++) {
+		const fruit = fruits[i];
+		if (isColliding(fruit, getPlayer())) {
+			score++;
+			fruits.splice(i, 1);
+			fruitsCollected++;
+			break;
+		}
+	}
+}
+}
 
-function collectFruit(event) {
-  event.target.remove();
-  score += 1;
-  fruitsCollected++;
-  scoreDisplay.textContent = `Score: ${score}`;
-  if (fruitsCollected === 20) {
-    scoreDisplay.textContent = `Score: ${score}`;
-    alert("Winner");
-    resetGame();
+function handleMouseDown(e) {
+// Fare tıklaması ile meyve toplama
+for (let i = 0; i < fruits.length; i++) {
+const fruit = fruits[i];
+if (isColliding(fruit, { x: e.offsetX, y: e.offsetY, width: 1, height: 1 })) {
+score++;
+fruits.splice(i, 1);
+fruitsCollected++;
+break;
+}
+}
+}
+
+function moveFruits(dx, dy) {
+for (let i = 0; i < fruits.length; i++) {
+const fruit = fruits[i];
+fruit.x += dx;
+fruit.y += dy;
+}
+}
+
+function getPlayer() {
+return {
+x: canvasWidth / 2 - 25,
+y: canvasHeight / 2 - 25,
+width: 50,
+height: 50
+};
+}
+
+function gameOver() {
+    document.getElementById("game-over").innerHTML = "Tebrikler! Oyunu kazandınız!";
   }
-}
+  
 
-function resetGame() {
-  score = 0;
-  fruitsCollected = 0;
-  scoreDisplay.textContent = `Score: ${score}`;
-  gameBoard.innerHTML = "";
-  startGame();
+function isColliding(obj1, obj2) {
+return (
+obj1.x < obj2.x + obj2.width &&
+obj1.x + obj1.width > obj2.x &&
+obj1.y < obj2.y + obj2.height &&
+obj1.y + obj1.height > obj2.y
+);
 }
-
-function addFruitToBoard() {
-  const fruit = createFruit();
-  gameBoard.appendChild(fruit);
-  setTimeout(addFruitToBoard, 1000);
-}
-
-function startGame() {
-  addFruitToBoard();
-}
-
-startGame();
